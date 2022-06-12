85e2ec08592e06d7e3f2153d9b62c037415fbc50
agung5114 <43102810+agung5114@users.noreply.github.com>
2022-06-12 17:06:00 +0700

    Add files via upload

--- grabfoodcv/stapp.py
+++ grabfoodcv/stapp.py
@@ -10,6 +10,51 @@ import joblib
 import operator
 import sys
 
+import requests
+from recipe_scrapers import scrape_html, scrape_me
+import re
+from pygsearch import gsearch
+
+# CSS to inject contained in a string
+hide_table_row_index = """
+            <style>
+            tbody th {display:none}
+            .blank {display:none}
+            </style>
+            """
+
+# Inject CSS with Markdown
+st.markdown(hide_table_row_index, unsafe_allow_html=True)
+
+# Display a static table
+
+def searchLink(foodname):
+    search = gsearch(f"{foodname} site=allrecipes.com",1)
+    result1 = str(search.results[0])
+    text = result1.split("link=",1)[1]
+    link = text.split(">",1)[0]
+    return link[1:-2]
+
+# rd = searchLink('rendang')
+# print(rd)
+
+def scrapeRecipe(url):
+    # url = f"https://www.allrecipes.com/recipe/72567/panna-cotta/"
+    html = requests.get(url).content
+    scraper = scrape_html(html=html, org_url=url)
+    # title = scraper.title()
+    # total_time=scraper.total_time()
+    # yields= scraper.yields()
+    ingredients =scraper.ingredients()
+    ingredient = []
+    for i in ingredients:
+        ingredient.append(re.sub (r'([^a-zA-Z ]+?)', '', i))
+
+    instructions = scraper.instructions()
+    # links = scraper.links()
+    # nutrients = scraper.nutrients()
+    return {'ingredient':ingredient,'ingredients':ingredients,'instructions':instructions}
+
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
 from tensorflow.keras.applications.mobilenet import preprocess_input
@@ -24,6 +69,7 @@ primaryColor="purple"
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
+            header {visibility: hidden;}
             footer {visibility: hidden;}
             </style>
             """
@@ -60,32 +106,42 @@ def getPrediction(data,model):
 # def main():
 st.subheader("Heal - Food Analyzer")
 with st.expander('Open Camera'):
-data1 = st.camera_input('')
+    data1 = st.camera_input('')
 with st.expander('Upload A Photo'):
-data2 = st.file_uploader('')
+    data2 = st.file_uploader('')
 
 if data1 != None:
-data = data1
+    data = data1
 elif data2 != None:
-data = data2
+    data = data2
 else:
-data = None
+    data = None
 
 if data == None:
-st.write('Please Upload Photo of Food')
+    st.write('Please Upload Photo of Food')
 else:
-img = Image.open(data)
-newsize = (280, 230)
-image = img.resize(newsize)
-st.image(image)
-
+    img = Image.open(data)
+    newsize = (280, 230)
+    image = img.resize(newsize)
+    c1,c2 = st.columns((1,1))
+    with c1:
+        st.image(image)
+    with c2:
 #     if st.button('Jalankan Prediksi'):
-hasil = getPrediction(data,model)
-hasil = hasil[['Food','Probability']]
-hasil.set_index('Food', inplace=True)
-st.table(hasil)
-# st.write(f'prediction: {hasil}')
-st.write(hasil.index[0].upper())
+        hasil = getPrediction(data,model)
+        hasil = hasil[['Food','Probability']]
+        hasil.set_index('Food', inplace=True)
+        st.table(hasil)
+    # st.write(f'prediction: {hasil}')
+    predicted = hasil.index[0]
+    st.write(predicted.upper())
+    link = searchLink(predicted)
+    st.write(link)
+    recipe = scrapeRecipe(link)
+    st.write('Ingredients')
+    st.table(recipe['ingredients'])
+    st.write('Instructions')
+    st.write(recipe['instructions'])
 
 # if __name__=='__main__':
 #     main()
