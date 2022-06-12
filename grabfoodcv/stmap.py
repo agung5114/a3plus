746122f3af7b1042124ec9523ec6833f7e916125
agung5114 <43102810+agung5114@users.noreply.github.com>
2022-06-12 23:05:37 +0700

    Update stmap.py

--- grabfoodcv/stmap.py
+++ grabfoodcv/stmap.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import plotly.express as px
+from urllib.request import urlopen
+import json
+import geopandas as gpd
+from geojson_rewind import rewind
+import streamlit as st
 
+f = gpd.read_file('jakarta.shp')
