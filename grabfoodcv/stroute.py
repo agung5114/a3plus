7adfc137129b20f039c36dcec0fc6f1ad9e1898e
agung5114 <43102810+agung5114@users.noreply.github.com>
2022-06-25 13:26:32 +0700

    Update stroute.py

--- grabfoodcv/stroute.py
+++ grabfoodcv/stroute.py
@@ -1,7 +1,7 @@
 import geopandas as gpd
 import networkx as nx
 # import geopy
-import matplotlib-inline.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import networkx.algorithms.approximation as nx_app
 # import math
