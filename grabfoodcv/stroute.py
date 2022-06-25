d30e5953de37b2b2f4b9e6a75be92343e5bccee1
agung5114 <43102810+agung5114@users.noreply.github.com>
2022-06-25 12:53:52 +0700

    Update stroute.py

--- grabfoodcv/stroute.py
+++ grabfoodcv/stroute.py
@@ -1,40 +1,20 @@
-import pandas as pd
-import plotly.express as px
-from urllib.request import urlopen
-import json
-import geopandas as gpd
-from geojson_rewind import rewind
-import streamlit as st
-st.set_page_config(
-     page_title="Demand Mapping",
-     page_icon="🧊",
-     layout="wide",
-     initial_sidebar_state="expanded"
- )
+def get_coordinates(cities):
+  coordinates = []
+  import http.client, urllib.parse
+  import json
+  conn = http.client.HTTPConnection('api.positionstack.com')
+  for city in cities:
+    params = urllib.parse.urlencode({
+        'access_key': '3327d83d1f4f3dcbd5b50579808e69e4',
+        'query': city,
+        'limit': 1,
+        })
 
-f = gpd.read_file('./grabfoodcv/jakarta.geojson')
-geo_ur = json.loads(f.to_json())
-map = rewind(geo_ur,rfc7946=False)
+    conn.request('GET', '/v1/forward?{}'.format(params))
 
-@st.cache
-def fetch_data(url):
-     data = pd.read_csv(url,dtype={"fips": str},sep=",")
-     return data
-
-dfall = fetch_data('./grabfoodcv/demand_jakarta.csv')
-# dfall = pd.read_csv('./grabfoodcv/demand_jakarta.csv',dtype={"fips": str},sep=",")
-df = dfall[dfall['GID_2']!='IDN.7.6_1']
-
-st.subheader('Regional Demand per Month')
-option = st.selectbox('Select Month', df['bulan'].unique())
-
-df_sel = df[df['bulan']==option]
-fig = px.choropleth(df_sel, geojson=map, locations='GID_4', color='Demand',
-                      featureidkey="properties.GID_4",
-                      color_discrete_sequence=None, 
-                      color_discrete_map={},
-                      color_continuous_scale='amp')
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-fig.update_geos(fitbounds="locations", visible=False)
-fig.update(layout_coloraxis_showscale=False)
-st.plotly_chart(fig)
+    res = conn.getresponse()
+    data = res.read()
+    data = data.decode('utf-8')
+    coordinate = (json.loads(data)['data'][0]['longitude'],json.loads(data)['data'][0]['latitude'])
+    coordinates.append(coordinate)
+  return coordinates
